68c312e927ca551d42ba249d626c094a57ea3518
ni-m-ta <tatsuyanisato@gmail.com>
2023-04-10 14:55:50 -0700

    done dll

--- dll.py
+++ dll.py
@@ -1,91 +1,112 @@
 class Node:
-    
-    # Initialization / Constructor -- specify variables for this object
+    # Node class to define each node in the doubly linked list
     def __init__(self, x):
-        self.__refNext = None # Reference to a potential next node, None by default
-        self.__refPre = None # Reference to a potential previous node, None by default
-        self.__value = x
-    
-    # Accessor methods
-    # a.k.a getters and setters
-    
+        self.__refNext = None # Initialize the reference to the next node as None
+        self.__refPre = None # Initialize the reference to the previous node as None
+        self.__value = x # Initialize the value of the node as x
+
+
     def getNext(self):
+        # Getter method to return the reference to the next node
         return self.__refNext
-        
+
     def setNext(self, r):
+        # Setter method to set the reference to the next node
         self.__refNext = r
 
     def getPre(self):
+        # Getter method to return the reference to the previous node
         return self.__refPre
 
     def setPre(self, r):
+        # Setter method to set the reference to the previous node
         self.__refPre = r
 
+    # Getter method to return the value of the node
     def getValue(self):
         return self.__value
-        
+
     def setValue(self, x):
+        # Setter method to set the value of the node
         self.__value = x
 
+
 class DLL:
-    
-    # Initialization / Constructor -- specify variables for this object
+    # Doubly linked list class
+
     def __init__(self):
-        self.__head = None # when we create the DLL we start it as an empty list
-        self.__tail = None # add a tail reference to end of list
-        
-    # Accessor methods - getters and setters
+        # Constructor to initialize the head and tail of the list as None
+        self.__head = None 
+        self.__tail = None
+
     def getHead(self):
+        # Getter method to return the head node of the list
         return self.__head
-        
+
+    def getTail(self):
+        # Getter method to return the tail node of the list
+        return self.__tail
+
     def setHead(self, h):
+        # Setter method to set the head node of the list
         self.__head = h
-        
-    # Methods to make the Singly Linked List work
+
+
     def add2Head(self, x):
+        # Method to add a new node to the head of the list
         if self.__head == None:
-            # add a new Node to the front of empty list
+            # If the list is empty, create a new node and set it as both head and tail
             newNode = Node(x)
             self.__head = newNode 
             self.__tail = newNode
-            #self.setHead(Node(x))
-        else: # add a new Node to the front of a list with existing Nodes
-            newNode = Node(x)   # create the newNode
+        else: 
+            # If the list is not empty, create a new node and set it as the head
+            newNode = Node(x)
             preHeadNode = self.__head
             newNode.setNext(preHeadNode)
             preHeadNode.setPre(newNode)
             self.__head = newNode
 
+
     def add2Tail(self, x):
+        # Method to add a new node to the tail of the list
         if self.__head is None:
+            # If the list is empty, add the node to the head
             self.add2Head(x)
         else:
+            # If the list is not empty, create a new node and set it as the tail
             newNode = Node(x)
             preTailNode = self.__tail
             preTailNode.setNext(newNode)
             newNode.setPre(preTailNode)
             self.__tail = newNode
 
+
     def insert(self, pos, y):
+        # Method to insert a new node at a specific position in the list
         traverser = self.__head
         counter = 0
         while  counter < pos-1:
             traverser = traverser.getNext()
             counter += 1
         preNode = traverser
+        nexNode = traverser.getNext()
         if self.__head is None:
             self.add2Head(y)
         elif self.__tail == preNode:
             self.add2Tail(y)
         else:
             newNode = Node(y)
-            newNode.setNext(preNode.getNext())
-            newNode.setPre(preNode)
+            nexNode.setPre(newNode)
+            newNode.setNext(nexNode)
             preNode.setNext(newNode)
+            newNode.setPre(preNode)
+
 
     def remove(self, x):
+        # Method to remove a node with a specific value from the list
         if self.__head is None:
-            print('It does not work')
+            print('List is empty')
         else:
             traverser = self.__head
             while traverser.getValue() != x:
@@ -93,39 +114,78 @@ class DLL:
                 traverser = traverser.getNext()
             nexNode = traverser.getNext()
             if x == self.__head.getValue():
-                self.__head == nexNode
+                nexNode.setPre(None)
+                self.__head = nexNode
             elif x == self.__tail.getValue():
-                self.__tail == preNode
+                preNode.setNext(None)
+                self.__tail = preNode
             else:
                 preNode.setNext(nexNode)
                 nexNode.setPre(preNode)
 
     def display(self):
-        # case: list is empty
+        # Method to display the list of nodes
         if self.__head is None:
             print("List is empty.")
-        else: #case: list has elements
-            print("->", end='')
+        else:
+            print("Display: ->", end='')
             traverser = self.__head
             while traverser is not None:
                 print(str(traverser.getValue())+"->", end='')
                 traverser = traverser.getNext()
             print("||")
-# # Test code
-# '''
-# newNode = Node(5)   # creation of the object -- is a call to the constructor or init method (line 11)
 
-# print(newNode.getvalue())
-# print(newNode.getNext())
-# '''
 
-d1 = DLL()  # Create a new Singly Linked List
+    def displayReverse(self):
+        # Method to display the list of reversed nodes
+        if self.__head is None:
+            print("List is empty")
+        else:
+            print("Reversely display: ||<-", end="")
+            traverser = self.__tail
+            while traverser is not None:
+                print(str(traverser.getValue())+"<-", end="")
+                preNode =  traverser.getPre()
+                traverser = preNode
+            print()
+            print('----------------------------------------------------------------')
 
-d1.display()
 
-d1.add2Head(3)
-d1.add2Head(4)
-d1.add2Tail(5)
-d1.insert(2,10)
-# d1.remove(4)
-d1.display()
+# Create a new doubly Linked List
+dll = DLL()
+
+# Test add2Head() function
+dll.add2Head(1)
+dll.add2Head(2)
+dll.add2Head(3)
+dll.display() # Expected output: ->3->2->1||
+dll.displayReverse() # Expected output: ||<-1<-2<-3
+
+# Test add2Tail() function
+dll.add2Tail(4)
+dll.add2Tail(5)
+dll.add2Tail(6)
+dll.display() # Expected output: ->3->2->1->4->5->6||
+dll.displayReverse() # Expected output: ||<-6<-5<-4<-1<-2<-3
+
+# Test insert() function
+dll.insert(3, 7)
+dll.display() # Expected output: ->3->2->1->7->4->5->6||
+dll.displayReverse() # Expected output: ||<-6<-5<-4<-7<-1<-2<-3
+
+# Test remove() function
+dll.remove(4)
+dll.display() # Expected output: ->3->2->1->7->5->6||
+dll.displayReverse() # Expected output: ||<-6<-5<-7<-1<-2<-3
+
+dll.remove(3)
+dll.display() # Expected output: ->2->1->7->5->6||
+dll.displayReverse() # Expected output: ||<-6<-5<-7<-1<-2
+
+dll.remove(6)
+dll.display() # Expected output: ->2->1->7->5||
+dll.displayReverse() # Expected output: ||<-5<-7<-1<-2
+
+# Test remove() function on empty list
+dll2 = DLL()
+dll2.remove(1) # Expected output: "List is empty."
